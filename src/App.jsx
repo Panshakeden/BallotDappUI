1a56ec6440280fd0cf2fc9201e4be73435042e10
panshak <panshakeden@gmail.com>
2024-03-14 12:54:40 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,37 @@
-import { Box, Container, Flex, Text } from "@radix-ui/themes";
+import { Container, Flex, Text } from "@radix-ui/themes";
 import { configureWeb3Modal } from "./connection";
 import "@radix-ui/themes/styles.css";
 import Header from "./component/Header";
 import Proposal from "./component/Proposal";
 import DelegateVote from "./component/DelegateVote";
 import useProposals from "./hooks/useProposals";
-import {
-    useWeb3ModalAccount,
-    useWeb3ModalProvider,
-} from "@web3modal/ethers/react";
-import { isSupportedChain } from "./utils";
-import { getProvider } from "./constants/providers";
-import { getProposalsContract } from "./constants/contracts";
+import useHandleVote from "./hooks/useHandleVote";
+import useDelegateVote from "./hooks/useDelegateVote";
+import { useState } from "react";
+import useNumberOfVoters from "./hooks/useNumberOfVoters";
 
 configureWeb3Modal();
 
 function App() {
     const { loading, data: proposals } = useProposals();
-    const { chainId } = useWeb3ModalAccount();
-    const { walletProvider } = useWeb3ModalProvider();
+    const [delegateAddress, setDelegateAddress] = useState("");
 
-    const handleVote = async (id) => {
-        if (!isSupportedChain(chainId)) return console.error("Wrong network");
-        const readWriteProvider = getProvider(walletProvider);
-        const signer = await readWriteProvider.getSigner();
-
-        const contract = getProposalsContract(signer);
-
-        try {
-            const transaction = await contract.vote(id);
-            console.log("transaction: ", transaction);
-            const receipt = await transaction.wait();
-
-            console.log("receipt: ", receipt);
-
-            if (receipt.status) {
-                return console.log("vote successfull!");
-            }
-
-            console.log("vote failed!");
-        } catch (error) {
-            console.log(error);
-            let errorText;
-            if (error.reason === "Has no right to vote") {
-                errorText = "You have not right to vote";
-            } else if (error.reason === "Already voted.") {
-                errorText = "You have already voted";
-            } else {
-                errorText = "An unknown error occured";
-            }
-
-            console.error("error: ", errorText);
-        }
-    };
+    const handleVote = useHandleVote();
+    const handleDelegateVote = useDelegateVote(delegateAddress);
+    const numberOfEligibleVoters = useNumberOfVoters();
 
     return (
         <Container>
             <Header />
             <main className="mt-6">
-                <Box mb="4">
-                    <DelegateVote />
-                </Box>
+                <Flex mb="4" justify="between">
+                    <DelegateVote
+                        delegateAddress={delegateAddress}
+                        setDelegateAddress={setDelegateAddress}
+                        handleDelegate={handleDelegateVote}
+                    />
+                    <span>Eligible Voters: {numberOfEligibleVoters}</span>
+                </Flex>
 
                 <Flex wrap={"wrap"} gap={"6"}>
                     {loading ? (
